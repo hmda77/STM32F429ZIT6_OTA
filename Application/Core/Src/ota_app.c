a37cc1e4f6192ec53b8fdb6263135e909b9fd355
Hamid <hamid.arabsorkhi77@gmail.com>
2023-10-26 17:40:46 +0330

    Add todo for etx_ota_download_and_flash

--- Application/Core/Src/ota_app.c
+++ Application/Core/Src/ota_app.c
@@ -19,18 +19,24 @@ void go_to_ota_app(UART_HandleTypeDef *huart)
 {
   /*Start the Firmware or Application update */
     printf("Starting Firmware Download!!!\r\n");
-//    if( etx_ota_download_and_flash(&huart5) != ETX_OTA_EX_OK )
+    if( etx_ota_download_and_flash(huart) != ETX_OTA_EX_OK )
+    {
       /* Error. Don't process. */
-//      printf("OTA Update : ERROR!!! HALT!!!\r\n");
-//      HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+      printf("OTA Update : ERROR!!! HALT!!!\r\n");
+      HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
     HAL_Delay(10000);
 //      HAL_NVIC_SystemReset();
-//    }
-//    else
-//    {
-//      /* Reset to load the new application */
-//      printf("Firmware update is done!!! Rebooting...\r\n");
-//      HAL_NVIC_SystemReset();
-//    }
-//  }
+    }
+    else
+    {
+      /* Reset to load the new application */
+      printf("Firmware update is done!!! Rebooting...\r\n");
+      HAL_NVIC_SystemReset();
+    }
+}
+
+ETX_OTA_EX_ etx_ota_download_and_flash(UART_HandleTypeDef *huart)
+{
+	// TODO:
+	return ETX_OTA_EX_OK;
 }
