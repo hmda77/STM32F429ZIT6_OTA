d870f891e041a660a704f6cb6e08e48372013e03
Hamid <hamid.arabsorkhi77@gmail.com>
2023-10-26 16:28:20 +0330

    Create ota App Library

---
+++ Application/Core/Inc/ota_app.h
@@ -0,0 +1,16 @@
+/*
+ * ota_app.h
+ *
+ *  Created on: 26 Oct 2023
+ *      Author: Hamid
+ */
+
+#ifndef INC_OTA_APP_H_
+#define INC_OTA_APP_H_
+
+#include "main.h"
+
+
+void go_to_ota_app();
+
+#endif /* INC_OTA_APP_H_ */
---
+++ Application/Core/Src/ota_app.c
@@ -0,0 +1,28 @@
+/*
+ * ota_app.c
+ *
+ *  Created on: 26 Oct 2023
+ *      Author: Hamid
+ */
+
+#include "ota_app.h"
+
+void go_to_ota_app()
+{
+  /*Start the Firmware or Application update */
+    printf("Starting Firmware Download!!!\r\n");
+//    if( etx_ota_download_and_flash(&huart5) != ETX_OTA_EX_OK )
+      /* Error. Don't process. */
+//      printf("OTA Update : ERROR!!! HALT!!!\r\n");
+//      HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+    HAL_Delay(10000);
+//      HAL_NVIC_SystemReset();
+//    }
+//    else
+//    {
+//      /* Reset to load the new application */
+//      printf("Firmware update is done!!! Rebooting...\r\n");
+//      HAL_NVIC_SystemReset();
+//    }
+//  }
+}
