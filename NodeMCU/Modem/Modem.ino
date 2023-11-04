735c996fe9de91e61d3b1dd468a2ba1f2b8c7e8d
Hamid <hamid.arabsorkhi77@gmail.com>
2023-11-04 16:13:15 +0330

    merge FW_info code with Modem

--- NodeMCU/Modem/Modem.ino
+++ NodeMCU/Modem/Modem.ino
@@ -1,23 +1,19 @@
-/**
-   StreamHTTPClient.ino
-
-    Created on: 24.05.2015
-
-*/
-
-#include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <SoftwareSerial.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <ArduinoJson.h>
 #include <FS.h>
 
 const char* ssid = "UwU";
 const char* password = "123hmd321";
-const char* fileUrl = "https://raw.githubusercontent.com/hmda77/STM32F429ZIT6_OTA/ModemCode/Application/Debug/Application.bin";
+
+const char* fwUrl = "https://fw.ziplast.ir/?getinfo";
 const char* filename = "/downloaded_file.bin";
 
+uint16_t crMajor = 1;
+uint32_t crMinor = 0;
 
 ESP8266WiFiMulti WiFiMulti;
 
@@ -25,8 +21,11 @@ ESP8266WiFiMulti WiFiMulti;
 // Add soft serial Port for OTA
 #define OTAPORT_TX 12
 #define OTAPORT_RX 13
+#define MAX_FW_INFO 500
 
 EspSoftwareSerial::UART otaPort;
+
+char fw_buf[MAX_FW_INFO];
 
 
 void setup() {
@@ -55,8 +54,9 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(ssid , password);
 
-      // Initialize file system
+  // Initialize file system
   SPIFFS.begin();
+
 }
 
 void readAndWriteFileToSerial(const char* filename) {
@@ -148,11 +148,107 @@ bool download_and_save(const char* url, const char* dest)
   return ret;
 }
 
+bool get_fw_info(const char* url, char* fw_buf)
+{
+  bool ret = false;
+
+  if ((WiFiMulti.run() == WL_CONNECTED))
+  {
+    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+
+    client->setInsecure();
+
+    HTTPClient https;
+
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, url)) {
+
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = https.getString();
+          strcpy(fw_buf, payload.c_str()); 
+          // Serial.println(payload);
+          ret = true;
+        }
+      } else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+  }
+  return ret;
+}
+
+
 void loop() {
 
-  if(download_and_save(fileUrl, filename))
+  memset(fw_buf, 0, sizeof(fw_buf));
+  if(get_fw_info(fwUrl, fw_buf))
   {
-    readAndWriteFileToSerial(filename);
+    // Serial.printf("%s\r\n",fw_buf);
+
+    StaticJsonDocument<96> doc;
+
+    DeserializationError error = deserializeJson(doc, fw_buf, MAX_FW_INFO);
+
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    int fw_version_minor = doc["fw_version"]["minor"]; // 0
+    int fw_version_major = doc["fw_version"]["major"]; // 5
+
+    const char* fw_crc = doc["fw_crc"]; //
+    long fw_size = doc["fw_size"];
+    const char*
+ fw_link = doc["fw_link"];
+
+
+    do
+    {
+      if ( !(crMajor <= fw_version_major) )
+      {
+        break;
+      }
+      
+      if ( (crMajor == fw_version_major))
+      {
+        if( !(crMinor < fw_version_minor) )
+        {
+          // Nothing for update
+          break;
+        }
+      }
+
+      Serial.println("NEW FIRMWARE!!!");
+      if(download_and_save(fw_link, filename))
+      {
+        readAndWriteFileToSerial(filename);
+
+        crMajor = fw_version_major;
+        crMinor = fw_version_minor;
+      }
+      else 
+      {
+        Serial.println("Download Failed");
+      }
+
+    }while(false);
+
   }
 
   Serial.println("Wait 10s before the next round...");
